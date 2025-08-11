dd4b2022bce86d910e82de3f21bba368881eafe6
Felipe <camargo963@gmail.com>
2025-08-11 08:07:31 -0300

    feat: Banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
 import Fastify from "fastify";
+import { knex } from "./database.js";
+import { env } from "./env/index.js";
 
 const app = Fastify();
 
-// GET, POST, PUT, PATH, DELETE
-app.get("/hello", () => {
-  return "Hello World";
+app.get("/hello", async () => {
+  const transaction = await knex("transactions")
+    .select("*")
+    .where("amount", 1000);
+
+  return transaction;
 });
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log("HTTP Server Running");
